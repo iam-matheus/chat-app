a3cb1ec839d8ecd44cefadab616fb348e9be60c8
Matheus Mulundu <61382592+iam-matheus@users.noreply.github.com>
2022-08-22 05:42:56 +0200

    created nodejs the server

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+//importing the http and CORS library to allow data transfer between the client and server domains
+const http = require("http").Server(app);
+const cors = require("cors");
+
+app.use(cors()); //instantiating the cors library
+
+//following, import socket.io to create a real time connection.
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost/3000",
+  },
+});
+
+/*
+From the code snippet below, the socket.io("connection") function
+establishes a connection with the React app, then creates a unique ID for
+each socket and logs the ID to the console whenever a user visits the web page.
+
+When you refresh or close the web page, the socket fires the disconnect event showing that a user has disconnected from the socket.
+*/
+
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+  });
+});
+
+//this code currently returns a json object when /api is requested
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Hello world",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to port: ${PORT}`);
+});
